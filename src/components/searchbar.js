251492eaa0d4f2b8e964488a60487252475681f9
Daniel Cuperman <dacooperfish@gmail.com>
2016-12-06 15:56:32 -0800

    searchbar basics functional, refactored to state component from functional component.

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -2,8 +2,26 @@ import React, { Component } from 'react';
 
 
 class SearchBar extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {term: ''};
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({term: event.target.value});
+  }
+
   render() {
-    return <input />;
+    return (
+      <div>
+        <input
+          onChange={this.onInputChange}
+          value = {this.state.term}
+          />;
+        Value of the Input: {this.state.term}
+      </div>
+    )
   }
 }
 
